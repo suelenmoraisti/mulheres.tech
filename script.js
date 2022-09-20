643b73db32150212021e5e3b513a378ea9ea5b90
suelenmoraisti <suelenmoraisti@gmail.com>
2022-09-20 13:44:23 -0300

    aula 19/09 atualizada

---
+++ script.js
@@ -0,0 +1,6 @@
+/**********************
+ * JavaScript do site *
+ **********************/
+
+/* REMOVER - Este código serve apenas para saber se o JavaScript está funcionando. */
+console.log('Mulheres.Tech');
