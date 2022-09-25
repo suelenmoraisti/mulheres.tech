af678f54ae23bc27145cd12a4cfd122f7c3e1843
André Luferat <33375030+Luferat@users.noreply.github.com>
2022-09-25 10:12:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-/**********************
- * JavaScript do site *
- **********************/
+/**
+ * JavaScript do aplicativo
+ * Depende de "jQuery" (https://jquery.com/)
+ *
+ * OBS: Este é o aplicativo principal, para que o tema (template) do site
+ * opere. Posteriormente, quando necessário, cada página (conteúdo) terá seu
+ * próprio JavaScript, assim, somente o JavaScript necessário será carregado.
+ *
+ * Para saber mais:
+ *   • https://www.w3schools.com/js/
+ *   • https://www.w3schools.com/jsref/
+ *   • https://www.w3schools.com/jquery/
+ */
 
-/* REMOVER - Este código serve apenas para saber se o JavaScript está funcionando. */
-console.log('Mulheres.Tech');
+// Executa aplicativo "runApp" quando o documento estiver pronto
+$(document).ready(runApp);
+
+function runApp() {
+  // Limpa mensagens do console
+  console.clear();
+
+  // Exibe uma mensagem no console do navegador
+  console.log("Mulheres.Tech");
+}
