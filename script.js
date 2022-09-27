e2b879c93b13d705cca5ca0aed033a42df11ad35
Luferat <andre@luferat.net>
2022-09-27 10:07:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,12 @@
  * JavaScript do aplicativo
  * Depende de "jQuery" (https://jquery.com/)
  *
- * OBS: Este é o aplicativo principal, para que o tema (template) do site
+ * OBS1: Este é o aplicativo principal, para que o tema (template) do site
  * opere. Posteriormente, quando necessário, cada página (conteúdo) terá seu
  * próprio JavaScript, assim, somente o JavaScript necessário será carregado.
+ * 
+ * OBS1: Todas as funções que iniciam com um cifrão ($) fazem parte da 
+ * biblioteca jQuery, ou seja, não são do JavaScript "puro" (vanilla).
  *
  * Para saber mais:
  *   • https://www.w3schools.com/js/
@@ -12,52 +15,86 @@
  *   • https://www.w3schools.com/jquery/
  */
 
-// Aplicativo principal
+/**
+ * runApp() → Aplicativo principal
+ * Este aplicativo é executado pela última linha deste código.
+ */
 function runApp() {
 
-  // Carrega a página inicial do site:
+  // Carrega a página inicial do site quando este iniciar:
   loadPage('home');
 
-  // Detecta cliques nos links (tags <a>)
+  /**
+   * jQuery → Quando houver click em um elemento <a>, execute o aplicativo 
+   * "routerLink":
+   **/
   $('a').click(routerLink);
 
 }
 
-// Aplicativo que executa clicques nos links:
+/**
+ * routerLink() → Aplicativo que processa cliques nos elementos <a>:
+ */
 function routerLink() {
 
-  // Lê o atributo "href" do link clicado e armazena em 'href':
+  /**
+   * jQuery → Recebe o atributo "href" do link clicado e armazena em 'href':
+   * A função "$(this)" faz referência ao elemento que foi clicado e disparou 
+   * este processo.
+   **/
   var href = $(this).attr('href');
 
-  // Detecta clique em links "http://" OU "https://" OU "#":
+  /**
+   * Se faz referência a link externo que começa com "http://" OU "https://",
+   * ou faz referência a uma âncora que começa com "#"...
+   * 
+   * OBS: O código "||" (pipe pipe) significa o "OU" (OR) lógico em JavaScript.
+   * 
+   * Referências: 
+   *    https://youtu.be/mp3g9IQ651g
+   *    https://www.w3schools.com/jsref/jsref_substring.asp
+   *    https://www.w3schools.com/js/js_if_else.asp
+   **/
   if (
-    // Se clicou em um link "http://..."...
+    // Se clicou em um link que começa com "http://...", OU
     href.substr(0, 7) == 'http://' ||
 
-    // Se clicou em um link "https://..."...
+    // Se clicou em um link que começa com "https://...", OU
     href.substr(0, 8) == 'https://' ||
 
-    // Se clicou em uma âncora...
+    // Se clicou em uma âncora que começa com "#"...
     href.substr(0, 1) == '#'
   ) {
 
-    // Retorna para o HTML abrir o link:
+    /**
+     * Encerra o programa retornando "true" (verdade) para que o HTML abra o 
+     * link normalmente:
+     **/
     return true;
   }
 
-  // Se clicou em um link interno (rota), executa o programa que carrega a página correta:
+  /**
+   * Se clicou em um link interno (rota para uma página), executa o programa 
+   * "loadPage()" que carrega a página correta:
+   **/
   loadPage(href);
 
-  // Bloqueia ação do HTML ao clicar no link
+  /**
+   * Encerra o programa retornando "false" (falso) para que a ação do HTML ao
+   * clicar no link seja bloqueada:
+   **/
   return false;
 }
 
-// Carrega a página correta do link:
+/**
+ * loadPage() → Aplicativo que processa os arquivos HTML, CSS e JavaScript da 
+ * "rota" solicitada e abre estes no site:
+ */
 function loadPage(href) {
 
   console.log(href);
 
 }
 
-// Executa aplicativo "runApp" quando o documento estiver pronto
+// jQuery → Executa aplicativo "runApp" quando o documento estiver pronto:
 $(document).ready(runApp);
